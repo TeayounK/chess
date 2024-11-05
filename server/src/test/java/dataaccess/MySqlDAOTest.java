e218910ccdd79477a5aebf76a81c470c5f9c01ce
Teayoun Kim <rkawk1@byu.edu>
2024-11-04 23:50:12 -0700

    Start to implement unit test

--- server/src/test/java/dataaccess/MySqlDAOTest.java
+++ server/src/test/java/dataaccess/MySqlDAOTest.java
@@ -1,4 +1,45 @@
 package dataaccess;
 
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.Service;
+
 public class MySqlDAOTest {
+    private Service service = new Service(new MemoryUserDAO(), new MemoryAuthDAO(), new MemoryGameDAO());
+
+    public void setting(){
+        DataAccessUser dataAccessUser = new MemoryUserDAO();
+        DataAccessAuth dataAccessAuth = new MemoryAuthDAO();
+        DataAccessGame dataAccessGame = new MemoryGameDAO();
+
+        try {
+            dataAccessUser = new MySqlDataAccessUser();
+            dataAccessAuth = new MySqlDataAccessAuth();
+            dataAccessGame = new MySqlDataAccessGame();
+
+        } catch(DataAccessException e) {
+            System.out.println(e.getMessage());
+        }
+        this.service = new Service(dataAccessUser, dataAccessAuth, dataAccessGame);
+    }
+
+    @BeforeEach
+    public void reset() throws DataAccessException {
+        setting();
+        service.clearDataBase();
+    }
+
+    @Test
+    public void addUserPositiveTest() {
+        Service service = new Service(new MemoryUserDAO(), new MemoryAuthDAO(), new MemoryGameDAO());
+        UserData newUser = new UserData("username","password","email");
+        try{
+            service.addUser(newUser);
+        }catch(DataAccessException e){
+            Assertions.assertNotNull(e,"addUser is not working");
+        }
+    }
 }
