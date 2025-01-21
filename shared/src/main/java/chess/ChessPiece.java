79877af16e3aaa4651b52c5e9acd829a4a01a09d
Teayoun Kim <rkawk1@byu.edu>
2025-01-21 10:08:33 -0700

    Chess piece done

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -33,14 +35,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -51,6 +53,48 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+
+        if (getPieceType() == PieceType.KING) {
+            KingMovementRule king = new KingMovementRule(board, myPosition);
+            return king.possibleMoves();
+        }else if ((getPieceType() == PieceType.KNIGHT)){
+            KnightMovementRule knight = new KnightMovementRule(board, myPosition);
+            return knight.possibleMoves();
+        }else if ((getPieceType() == PieceType.PAWN)){
+            PawnMovementRule pawn = new PawnMovementRule(board, myPosition);
+            return pawn.possibleMoves();
+        }else if ((getPieceType() == PieceType.QUEEN)){
+            ArrayList<ChessMove> queensMove = (ArrayList<ChessMove>) GeneralChessMoves.straightMoves(board,myPosition);
+            queensMove.addAll(GeneralChessMoves.diagnalMoves(board,myPosition));
+            return queensMove;
+        }else if ((getPieceType() == PieceType.ROOK)){
+            return GeneralChessMoves.straightMoves(board,myPosition);
+        }else if ((getPieceType() == PieceType.BISHOP)){
+            return GeneralChessMoves.diagnalMoves(board,myPosition);
+        }else{
+            return new ArrayList<>();
+        }
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {return true;}
+        if (o == null || getClass() != o.getClass()) {return false;}
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessPiece{" +
+                "pieceColor=" + pieceColor +
+                ", type=" + type +
+                '}';
     }
 }
