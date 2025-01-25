292e029319a29f19228e5012ce2aa0baeb896373
Teayoun Kim <rkawk1@byu.edu>
2025-01-24 20:28:02 -0700

    King and Knight movement rules

--- shared/src/main/java/chess/KingMovementRule.java
+++ shared/src/main/java/chess/KingMovementRule.java
@@ -1,4 +1,44 @@
 package chess;
 
+import java.util.Collection;
+import java.util.Objects;
+
 public class KingMovementRule {
+
+    private ChessBoard board;
+    private ChessPosition position;
+    public KingMovementRule(ChessBoard board, ChessPosition position){
+        this.board = board;
+        this.position = position;
+    }
+
+    public Collection<ChessMove> possibleMoves(){
+        int [][] possible = {{position.getRow(), position.getColumn()-1},
+                {position.getRow(), position.getColumn()+1},
+                {position.getRow()+1, position.getColumn()},
+                {position.getRow()+1, position.getColumn()-1},
+                {position.getRow()+1, position.getColumn()+1},
+                {position.getRow()-1, position.getColumn()},
+                {position.getRow()-1, position.getColumn()-1},
+                {position.getRow()-1, position.getColumn()+1}};
+
+        return GeneralChessMoves.getChessMoves(possible,board,position);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        KingMovementRule that = (KingMovementRule) o;
+        return Objects.equals(board, that.board) && Objects.equals(position, that.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(board, position);
+    }
 }
--- shared/src/main/java/chess/KnightMovementRule.java
+++ shared/src/main/java/chess/KnightMovementRule.java
@@ -1,4 +1,52 @@
 package chess;
 
+import java.util.Collection;
+import java.util.Objects;
+
 public class KnightMovementRule {
+
+    private ChessBoard board;
+    private ChessPosition position;
+    public KnightMovementRule(ChessBoard board, ChessPosition position){
+        this.board = board;
+        this.position = position;
+    }
+    public Collection<ChessMove> possibleMoves(){
+
+        int [][] possible = {{position.getRow()+2, position.getColumn()-1},
+                {position.getRow()+2, position.getColumn()+1},
+                {position.getRow()-2, position.getColumn()-1},
+                {position.getRow()-2, position.getColumn()+1},
+                {position.getRow()+1, position.getColumn()+2},
+                {position.getRow()-1, position.getColumn()+2},
+                {position.getRow()+1, position.getColumn()-2},
+                {position.getRow()-1, position.getColumn()-2}};
+
+        return GeneralChessMoves.getChessMoves(possible,board,position);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        KnightMovementRule that = (KnightMovementRule) o;
+        return Objects.equals(board, that.board) && Objects.equals(position, that.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(board, position);
+    }
+
+    @Override
+    public String toString() {
+        return "KnightMovementRule{" +
+                "board=" + board +
+                ", position=" + position +
+                '}';
+    }
 }
