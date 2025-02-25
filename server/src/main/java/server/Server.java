28cb50749610bea7caf76ea1b9aeec68b092b11e
Teayoun Kim <rkawk1@byu.edu>
2025-02-24 22:47:03 -0700

    Start Server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,25 +1,7 @@
 package server;
 
-import spark.*;
+import service.Service;
 
 public class Server {
-
-    public int run(int desiredPort) {
-        Spark.port(desiredPort);
-
-        Spark.staticFiles.location("web");
-
-        // Register your endpoints and handle exceptions here.
-
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
-
-        Spark.awaitInitialization();
-        return Spark.port();
-    }
-
-    public void stop() {
-        Spark.stop();
-        Spark.awaitStop();
-    }
+    private Service service = new Service(new MemoryUserDAO(), new MemoryAuthDAO(), new MemoryGameDAO());
 }
