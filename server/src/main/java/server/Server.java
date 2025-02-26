0976f5e849ea47c4b81236661392e1df152466cc
Teayoun Kim <rkawk1@byu.edu>
2025-02-25 23:15:00 -0700

    Server DAOs

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,7 +1,31 @@
 package server;
 
 import service.Service;
+import spark.Spark;
 
 public class Server {
     private Service service = new Service(new MemoryUserDAO(), new MemoryAuthDAO(), new MemoryGameDAO());
+
+    public int run(int desiredPort) {
+        DataAccessUser dataAccessUser = new MemoryUserDAO();
+        DataAccessAuth dataAccessAuth = new MemoryAuthDAO();
+        DataAccessGame dataAccessGame = new MemoryGameDAO();
+
+        this.service = new Service(dataAccessUser, dataAccessAuth, dataAccessGame);
+
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+        //This line initializes the server and can be removed once you have a functioning endpoint
+        Spark.init();
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+
 }
