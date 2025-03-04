913db34d4a45cd450ca61408239c22398166ed7a
Teayoun Kim <rkawk1@byu.edu>
2025-03-03 21:15:03 -0700

    createGame, getGame, clearAll done in MemoryGameDAO

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,4 +1,35 @@
 package dataaccess;
 
-public class MemoryGameDAO {
+import model.GameData;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+public class MemoryGameDAO implements DataAccessGame{
+    private HashMap<Integer, GameData> games = new HashMap<>();
+    int maxID = 0;
+
+
+    @Override
+    public GameData createGame(GameData gameData) throws DataAccessException {
+        if (gameData.gameName() == null){
+            throw new DataAccessException("Error: Not a valid Game name");
+        }else {
+            maxID += 1;
+            GameData newGame = new GameData(maxID, null,null,gameData.gameName(),null);
+            games.put(maxID, newGame);
+            return newGame;
+        }
+    }
+
+    public Collection<GameData> getGames(){
+        return games.values();
+    }
+
+    @Override
+    public void clearAll() {
+        games = new HashMap<>();
+        maxID = 0;
+    }
+
 }
