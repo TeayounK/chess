9de8c940efa51b97fb3e534431c6b5c8be8b8656
Teayoun Kim <rkawk1@byu.edu>
2025-03-03 21:18:55 -0700

    AddUser, GetUser, clearAll, VerifyUser done in MemoryUserDAO

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,4 +1,30 @@
 package dataaccess;
 
-public class MemoryUserDAO {
+import java.util.HashMap;
+
+public class MemoryUserDAO implements DataAccessUser {
+    private HashMap<String, UserData> users = new HashMap<>();
+
+
+    @Override
+    public void addUser(UserData user) throws DataAccessException {
+        if (user.username() == null){
+            throw new DataAccessException("Error: Not a valid ID");
+        }else {
+            users.put(user.username(), user);
+        }
+    }
+
+    @Override
+    public UserData getUser(String username){
+        return users.get(username);
+    }
+
+    public void clearAll() throws DataAccessException{
+        users = new HashMap<>();
+    }
+    public boolean verifyUser(String username, String password){
+        return false;
+    }
+
 }
