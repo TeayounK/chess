a07289e1b061a4f5a6dc484b195d116fe053bc72
Teayoun Kim <rkawk1@byu.edu>
2025-03-03 21:09:28 -0700

    getUser, removeAuth done in MemoryAuthDAO

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -22,4 +22,20 @@ public class MemoryAuthDAO {
         }
         return null;
     }
+    @Override
+    public String getUser(String authToken){
+        return authsKey.get(authToken).username();
+    }
+
+    @Override
+    public void removeAuth(String authToken) throws DataAccessException {
+        if (authsKey.get(authToken)==null){
+            throw new DataAccessException("Error: Already logged-out username");
+        }
+        String theUser = authsKey.get(authToken).username();
+        if (theUser == null) {throw new DataAccessException("Error: Already logged-out username");}
+        else {
+            authsKey.remove(authToken);
+        }
+    }
 }
