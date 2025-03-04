6d9220cad0952ab3a5e1b038ba53c095fd68e91e
Teayoun Kim <rkawk1@byu.edu>
2025-03-03 21:08:45 -0700

    AddAuth, getAuth done in MemoryAuthDAO

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -3,7 +3,23 @@ package dataaccess;
 import model.AuthData;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class MemoryAuthDAO {
     private HashMap<String, AuthData> authsKey = new HashMap<>();
+
+    @Override
+    public void addAuth(AuthData authData) {
+        authsKey.put(authData.authToken(),authData);
+    }
+
+    @Override
+    public AuthData getAuth(String username){
+        for (AuthData authData:authsKey.values()){
+            if (Objects.equals(authData.username(), username)){
+                return authData;
+            }
+        }
+        return null;
+    }
 }
