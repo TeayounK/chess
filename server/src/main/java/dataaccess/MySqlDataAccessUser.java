918c15b6c139f1782b5aff5774d47553b2552690
Teayoun Kim <rkawk1@byu.edu>
2025-03-12 21:22:17 -0600

    MySqlDataAccessAuth String and configureDatabase

--- server/src/main/java/dataaccess/MySqlDataAccessUser.java
+++ server/src/main/java/dataaccess/MySqlDataAccessUser.java
@@ -1,4 +1,18 @@
 package dataaccess;
 
-public class MySqlDataAccessUser {
+public class MySqlDataAccessAuth implements DataAccessAuth{
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  authsKey (
+              `authToken` varchar(256) NOT NULL,
+              `username` varchar(256) NOT NULL,
+              PRIMARY KEY (`authToken`)
+            )
+            """
+    };
+
+    public MySqlDataAccessAuth() throws DataAccessException {
+        DatabaseManager.configureDatabase(createStatements);
+    }
 }
