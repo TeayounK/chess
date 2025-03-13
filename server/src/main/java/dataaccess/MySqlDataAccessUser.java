9292649d16fbf875fc462f28d65b62897bc16bcf
Teayoun Kim <rkawk1@byu.edu>
2025-03-12 21:29:51 -0600

    MySqlDataAccessUser implement storeUserPassword, verifyUser

--- server/src/main/java/dataaccess/MySqlDataAccessUser.java
+++ server/src/main/java/dataaccess/MySqlDataAccessUser.java
@@ -1,6 +1,7 @@
 import dataaccess.DataAccessException;
 import dataaccess.DataAccessUser;
 import dataaccess.DatabaseManager;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class MySqlDataAccessUser implements DataAccessUser {
 
@@ -17,5 +18,16 @@ public class MySqlDataAccessUser implements DataAccessUser {
 
     public MySqlDataAccessUser() throws DataAccessException {
         DatabaseManager.configureDatabase(createStatements);
+    }
+
+    private String storeUserPassword(String clearTextPassword) {
+        return BCrypt.hashpw(clearTextPassword, BCrypt.gensalt());
+    }
+
+    public boolean verifyUser(String username, String providedClearTextPassword){
+        // read the previously hashed password from the database
+        var hashedPassword = readHashedPasswordFromDatabase(username);
+
+        return BCrypt.checkpw(providedClearTextPassword, hashedPassword);
     }
 }
