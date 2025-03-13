77cc054e632918c157978487141fb4df29a3db58
Teayoun Kim <rkawk1@byu.edu>
2025-03-12 21:33:19 -0600

    MySqlDataAccessUser override addUser, getUser, clearAll

--- server/src/main/java/dataaccess/MySqlDataAccessUser.java
+++ server/src/main/java/dataaccess/MySqlDataAccessUser.java
@@ -1,6 +1,7 @@
 import dataaccess.DataAccessException;
 import dataaccess.DataAccessUser;
 import dataaccess.DatabaseManager;
+import model.UserData;
 import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.SQLException;
@@ -50,5 +51,53 @@ public class MySqlDataAccessUser implements DataAccessUser {
             throw new RuntimeException(e);
         }
         return null;
+    }
+
+    @Override
+    public void addUser(UserData user) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.setString(1,user.username());
+                preparedStatement.setString(2,storeUserPassword(user.password()));
+                preparedStatement.setString(3,user.email());
+                preparedStatement.executeUpdate();
+            }
+
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable register user: %s", ex.getMessage()));
+        }
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT * FROM users WHERE username=?";
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.setString(1,username);
+                try(var result = preparedStatement.executeQuery()){
+                    if (result.next()){
+                        var password = result.getString("password");
+                        var email = result.getString("email");
+                        return new UserData(username,password,email);
+                    }
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to get the user data: %s", ex.getMessage()));
+        }
+        return null;
+    }
+
+    @Override
+    public void clearAll() throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "TRUNCATE users";
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to clean up the database: %s", ex.getMessage()));
+        }
     }
 }
