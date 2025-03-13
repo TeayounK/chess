a5168fa7b0893061f20467e463157b1868a18a82
Teayoun Kim <rkawk1@byu.edu>
2025-03-12 21:36:29 -0600

    MySqlDataAccessGame initialize it

--- server/src/main/java/dataaccess/MySqlDataAccessGame.java
+++ server/src/main/java/dataaccess/MySqlDataAccessGame.java
@@ -1,4 +1,21 @@
 package dataaccess;
 
-public class MySqlDataAccessGame {
+public class MySqlDataAccessGame implements DataAccessGame {
+    private int maxID = 0;
+
+    public MySqlDataAccessGame() throws DataAccessException {
+        String[] createStatements = {
+                """
+            CREATE TABLE IF NOT EXISTS  games (
+              `gameID` INT NOT NULL,
+              `whiteUsername` varchar(256) NULL,
+              `blackUsername` varchar(256) NULL,
+              `gameName` varchar(256) NOT NULL,
+              `game` TEXT NULL,
+              PRIMARY KEY (`gameID`)
+            )
+            """
+        };
+        DatabaseManager.configureDatabase(createStatements);
+    }
 }
