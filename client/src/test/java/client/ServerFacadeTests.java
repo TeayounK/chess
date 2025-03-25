57ada7630837ff736683fdf27c8bf5804f712d08
Teayoun Kim <rkawk1@byu.edu>
2025-03-25 14:45:15 -0600

    Implement test case for register in ServerFacadeTests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,17 +1,26 @@
 package client;
 
+import model.GameData;
+import model.JoinGame;
+import model.ListResult;
 import org.junit.jupiter.api.*;
 import server.Server;
+import ui.ResponseException;
+import ui.ServerFacade;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
+        facade = new ServerFacade("http://localhost:"+port);
         System.out.println("Started test HTTP server on " + port);
     }
 
@@ -22,8 +31,11 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void registerPositive() throws Exception {
+        facade.deleteDataBase();
+        var authData = facade.createUser("player1", "password", "p1@email.com");
+        assertTrue(authData.authToken().length() > 10);
     }
+
 
 }
