f55deea5cffafe6ca0a945cd25fa3b8614f6acc3
Teayoun Kim <rkawk1@byu.edu>
2025-03-25 14:45:53 -0600

    Implement test case for login in ServerFacadeTests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -37,5 +37,42 @@ public class ServerFacadeTests {
         assertTrue(authData.authToken().length() > 10);
     }
 
+    @Test
+    void registerNegative() throws Exception {
+        try{
+            facade.deleteDataBase();
+            var authData = facade.createUser("player1", "password", null);
+            assertTrue(authData.authToken().length() > 10);
+        }catch(ResponseException e){
+            Assertions.assertEquals(e.getMessage(),"failure: 400 Bad Request");
+        }
+    }
+
+    @Test
+    void loginPositive() throws Exception{
+        facade.deleteDataBase();
+        var authData = facade.createUser("player1", "password", "p1@email.com");
+        try{
+            var newAuthData = facade.loginUser("player1", "password");
+            assertTrue(newAuthData.authToken().length() > 10);
+        }catch(ResponseException e){
+            throw new Exception("login failed");
+        }
+    }
+
+    @Test
+    void loginNegative() throws Exception{
+        facade.deleteDataBase();
+        var authData = facade.createUser("player1", "password", "p1@email.com");
+        try{
+            var newAuthData = facade.loginUser("player1", "wrongPassword");
+            assertTrue(newAuthData.authToken().length() > 10);
+        }catch(ResponseException e){
+            Assertions.assertEquals(e.getMessage(),"failure: 401 Unauthorized");
+        }
+    }
+
+
+
 
 }
