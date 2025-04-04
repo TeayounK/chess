9c2e8145c1c17a6def2e862a611a5aad0f4a9193
Teayoun Kim <rkawk1@byu.edu>
2025-04-03 22:19:46 -0600

    Start Repl

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,4 +1,17 @@
 package ui;
 
-public class Repl {
+import websocket.messages.ServerMessage;
+
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
+public class Repl implements NotificationHandler {
+    private final ChessClient client;
+    private States state;
+
+    public Repl(String serverUrl) {
+        client = new ChessClient(serverUrl);
+        this.state = States.PRELOGIN;
+    }
 }
