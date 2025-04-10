66729349aca61c2a03ac734142565e8fa378711b
Teayoun Kim <rkawk1@byu.edu>
2025-04-09 23:05:14 -0600

    implement run method in Repl

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 import static ui.EscapeSequences.*;
 
-public class Repl implements NotificationHandler {
+public class Repl implements NotificationHandler{
     private final ChessClient client;
     private States state;
 
@@ -14,4 +14,33 @@ public class Repl implements NotificationHandler {
         client = new ChessClient(serverUrl);
         this.state = States.PRELOGIN;
     }
+
+    public void run(){
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+
+        while (!result.equals("quit")) {
+            System.out.println(switch(this.state){
+                case States.PRELOGIN -> printPromptPreLogin();
+                case States.LOGIN -> printPromptLogin();
+                case States.GAME -> printPromptGame();
+            });
+
+            String line = scanner.nextLine();
+
+//            do I need to separate two cases whether we want to do websocket or http?
+
+            try {
+                result = client.eval(line);
+                this.state = client.state;
+                System.out.print(result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+
 }
